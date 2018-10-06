f407f7a184005f070a62b8910fff98be152906ef
Marc Neander <marc.neander@schibsted.se>
2018-10-06 17:08:49 +0200

    Use my own eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,5 @@
 module.exports = {
-    parser: 'babel-eslint',
     extends: [
-        'airbnb',
-        'prettier',
-        'prettier/react',
-    ],
-    plugins: ['prettier'],
-    env: {
-        browser: true
-    },
-    rules: {
-        'import/no-extraneous-dependencies': 'off',
-        'comma-dangle': 'off',
-        'no-underscore-dangle': 'off',
-
-        // jsx-a11y
-        "jsx-a11y/click-events-have-key-events": 'off',
-
-        // react
-        "react/forbid-prop-types": 'off', // only here
-        'react/jsx-filename-extension': 'off',
-        'react/prefer-stateless-function': 'off',
-        "react/destructuring-assignment": 'never'
-    }
+        '@marcneander/react'
+    ]
 };
