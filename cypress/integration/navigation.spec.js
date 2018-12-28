8082b67d7f645f9bc3b7f18c640726cda62a73c6
Marc Neander <marcneander@gmail.com>
2018-12-28 23:35:11 +0100

    Test fixes

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -35,24 +35,24 @@ context('Navigation', () => {
             cy.visit('http://localhost:8000');
 
             cy.get('@offcanvasBtn').click();
-            cy.get('[data-cy=offcanvas-contact-link]')
-                .wait(200)
+            cy.wait(300)
+                .get('[data-cy=offcanvas-contact-link]')
                 .click();
             cy.location('pathname').should('include', 'contact');
         });
 
         it('can navigate to profile', () => {
             cy.get('@offcanvasBtn').click();
-            cy.get('[data-cy=offcanvas-profile-link]')
-                .wait(200)
+            cy.wait(300)
+                .get('[data-cy=offcanvas-profile-link]')
                 .click();
             cy.location('pathname').should('include', 'profile');
         });
 
         it('can navigate to home', () => {
             cy.get('@offcanvasBtn').click();
-            cy.get('[data-cy=offcanvas-home-link]')
-                .wait(200)
+            cy.wait(300)
+                .get('[data-cy=offcanvas-home-link]')
                 .click();
             cy.location('pathname').should('eq', '/');
         });
